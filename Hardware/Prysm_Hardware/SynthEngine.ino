b046ed394684b2bd6b1216a0ab3a826a97a9b96e
Dom D'Attilio <ddattilio20@gmail.com>
2022-11-01 12:22:12 -0500

    added key lookup stuff
    added key array and function to lookup key value based on pin input

--- Hardware/Prysm_Hardware/SynthEngine.ino
+++ Hardware/Prysm_Hardware/SynthEngine.ino
@@ -1,13 +1,13 @@
 #include <SPI.h>
-
 using namespace std;
 
-void masking()
+char keyValues[18] = {"C","C#","D","D#","E","F","F#","G","G#","A","A#","B","C","C#","D","D#","E","F"};
+
+char getKey(int pinVal)
 {
-    
+  pinVal = pinVal-20;
+  return keyValues[pinVal % 18];
 }
-
-
 
 void setup() {
   Serial.begin(9600);
