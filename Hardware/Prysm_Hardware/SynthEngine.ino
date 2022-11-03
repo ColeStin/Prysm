e6d86c1710cbccb937e2f585a3111b13af1bc158
Alex Wilhelm <alexwilhelm@ku.edu>
2022-11-03 14:50:14 -0500

    Digital Pin Setup Done

--- Hardware/Prysm_Hardware/SynthEngine.ino
+++ Hardware/Prysm_Hardware/SynthEngine.ino
@@ -10,11 +10,38 @@ char getKey(int pinVal)
 }
 
 void setup() {
+  
   Serial.begin(9600);
 
- 
+  // for (int pinNumber = 20, pinNumber < 38, pinNumber++) //Sets up PIN Numbers 20 - 37
+  // {
+  //  pinMode(pinNumber, INPUT_PULLUP);
+  // }
+
+  //USE FOR LOOP LONG TERM
+
+  pinMode(20, INPUT_PULLUP);
+  pinMode(21, INPUT_PULLUP);
+  pinMode(22, INPUT_PULLUP);
+  pinMode(23, INPUT_PULLUP);
+  pinMode(24, INPUT_PULLUP);
+  pinMode(25, INPUT_PULLUP);
+  pinMode(26, INPUT_PULLUP);
+  pinMode(27, INPUT_PULLUP);
+  pinMode(28, INPUT_PULLUP);
+  pinMode(29, INPUT_PULLUP);
+  pinMode(30, INPUT_PULLUP);
+  pinMode(31, INPUT_PULLUP);
+  pinMode(32, INPUT_PULLUP);
+  pinMode(33, INPUT_PULLUP);
+  pinMode(34, INPUT_PULLUP);
+  pinMode(35, INPUT_PULLUP);
+  pinMode(36, INPUT_PULLUP);
+  pinMode(37, INPUT_PULLUP);
+
 }
 
-void loop() {
+void loop() 
+{
 
 }
