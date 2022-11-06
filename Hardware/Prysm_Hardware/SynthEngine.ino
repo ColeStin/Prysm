ce1b6f5b14d47d5a3b27d4972985e0cbd55f961b
ColeStin <60800520+ColeStin@users.noreply.github.com>
2022-11-06 13:19:22 -0600

    Checks and Balances
    this commit fixed :
     - the error with the char array "keyValues" because it was originally an array of strings
     - the issue with the outputing of a bool array, println was not a fan so it will now create a string and populate it with all the pin values that are "TRUE" within the bool array "values"
     - the addition of green code

--- Hardware/Prysm_Hardware/SynthEngine.ino
+++ Hardware/Prysm_Hardware/SynthEngine.ino
@@ -1,7 +1,12 @@
 #include <SPI.h>
+#include <string.h> //this library is used for test purposes only
+
 using namespace std;
 
-char keyValues[18] = {"C","C#","D","D#","E","F","F#","G","G#","A","A#","B","C","C#","D","D#","E","F"};
+
+//sharp keys are designated by upper case letters where as lower case letters are normal values
+//this is done because we wanted to keep them as chars so they did not take up too much RAM
+char keyValues[18] = {'c','C','d','D','e','f','F','g','G','a','A','b','c','C','d','D','e','f'};
 
 char getKey(int pinVal)
 {
@@ -43,5 +48,33 @@ void setup() {
 
 void loop() 
 {
+    int amount_of_vals = 18; //used for testing purposes
+    int start_pin = 20; //used for testing purposes
+    bool values[amount_of_vals] = {false}; //delcare an array of bools and set them all to false
+    for(int i = start_pin; i<(start_pin + amount_of_vals); i++){//start a loop that will loop through all pin numbers
 
+      //look at the current pin (20-38) and see if it is reading
+      //set the bool value to the array instance for that pin (0-17)
+      values[i-start_pin] = digitalRead(i) == LOW; 
+    }
+    
+    //this section is for testing purposes only!!!!
+    //it is used to generate a string to output to the terminal so that
+    //we can see all the buttons being pressed.
+    //it will more than double the run time of the program, so when demo-ing,
+    //that needs to be accounted for
+    String output= "[";
+    for(int i = 0;i<amount_of_vals; i++){
+      if(values[i] == "1"){
+        //this if statement reads the value of the individual "key" within the array
+        //that was generated in the previous loop, and adds its value to a string to output 
+        //whether or not it was pushed
+
+        //this will calculate the exact pin number and not just the instance in the loop 
+        output = output + (i+start_pin) + ", ";
+      }
+    }
+    output = output + "END]";
+    //output the string to see which keys are being pressed
+    Serial.println(output);
 }
