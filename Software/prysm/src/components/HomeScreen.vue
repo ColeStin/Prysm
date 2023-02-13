2193eedf0ff6ed4fa1642d0757ffc25f791ed9fc
Gage B <gsburmaster@ku.edu>
2023-02-13 11:37:45 -0600

    cleaned up some of the UI

--- Software/prysm/src/components/HomeScreen.vue
+++ Software/prysm/src/components/HomeScreen.vue
@@ -43,25 +43,7 @@
 
     <div class="point-adjustment">
 
-      <!-- Creates a point in the center of the graph-->
-      <div class="add-point" @click="addPoint">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-node-plus"
-          viewBox="0 0 16 16">
-          <path fill-rule="evenodd"
-            d="M11 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8zM6.025 7.5a5 5 0 1 1 0 1H4A1.5 1.5 0 0 1 2.5 10h-1A1.5 1.5 0 0 1 0 8.5v-1A1.5 1.5 0 0 1 1.5 6h1A1.5 1.5 0 0 1 4 7.5h2.025zM11 5a.5.5 0 0 1 .5.5v2h2a.5.5 0 0 1 0 1h-2v2a.5.5 0 0 1-1 0v-2h-2a.5.5 0 0 1 0-1h2v-2A.5.5 0 0 1 11 5zM1.5 7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1z" />
-        </svg>
-        Add Point
-      </div>
-
-      <!-- Deletes a selected point-->
-      <div class="delete-point" @click="deletePoint">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-node-minus"
-          viewBox="0 0 16 16">
-          <path fill-rule="evenodd"
-            d="M11 4a4 4 0 1 0 0 8 4 4 0 0 0 0-8zM6.025 7.5a5 5 0 1 1 0 1H4A1.5 1.5 0 0 1 2.5 10h-1A1.5 1.5 0 0 1 0 8.5v-1A1.5 1.5 0 0 1 1.5 6h1A1.5 1.5 0 0 1 4 7.5h2.025zM1.5 7a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1zM8 8a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5A.5.5 0 0 1 8 8z" />
-        </svg>
-        Delete Point
-      </div>
+      
       <!-- Seperates New, Open, and Save File from Above Row-->
       <div class="new-open-save">
         <!-- Resets the wavetable to original state-->
@@ -93,12 +75,11 @@
           </svg>
         </div>
 
-        <div class="nameFile-button" @click="nameFile()">Name File  </div> <input type='text' v-model="inputName" />
+        Name File <input type='text' v-model="file" />
       </div>
       <div class="function-input">
         <div class="input-boxes">
           Function: <input type='text' v-model="functionIn" />
-          Range: <input type="text" v-model="inputRange" />
           Minimum: <input type="text" v-model="inputMin" />Maximum: <input type="number" v-model="inputMax" /> 
           <!-- <button @click="addFunction()">submit</button> -->
           <div class="submit-button" @click="addFunction()">Submit
