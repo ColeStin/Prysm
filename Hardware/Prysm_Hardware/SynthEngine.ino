f6d9efa18610c27289cd7540a17a88c43bba3d32
Dom D'Attilio <46716143+ddattilio20@users.noreply.github.com>
2023-03-07 17:25:27 -0600

    Update SynthEngine.ino

--- Hardware/Prysm_Hardware/SynthEngine.ino
+++ Hardware/Prysm_Hardware/SynthEngine.ino
@@ -45,6 +45,7 @@ class Oscillator{
           const auto sample = interpolate();
           index += indexIncrement;
          }
+         return sample;
       }
 
       //stops sampling, resets indexes to 0
